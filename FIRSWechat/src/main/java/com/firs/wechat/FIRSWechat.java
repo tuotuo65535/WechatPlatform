61caab0002aa9cacde57f9c694577f924162c84c
tuotuo65535 <562182858@qq.com>
2018-05-16 14:15:17 +0800

    wx-tools框架spring boot初始化

---
+++ FIRSWechat/src/main/java/com/firs/wechat/FIRSWechat.java
@@ -0,0 +1,12 @@
+package com.firs.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FIRSWechat {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FIRSWechat.class, args);
+	}
+}
